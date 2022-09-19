87573092286a5738c16be4e888a0bf5ac5c000ae
vhillyjay <107127737+vhillyjay@users.noreply.github.com>
2022-09-19 17:12:32 +0800

    controller for blogs

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,63 +1,15 @@
 const express = require('express');
-const Blog = require('../models/blog');
+// const Blog = require('../models/blog');
+const blogController = require('../controllers/blogController');
 const router = express.Router();
 
-router.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create' });
-});
-router.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1})
-        .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-router.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-router.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then((result) => {
-            res.render('details', { title: id, blogs: result });
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
-router.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            res.json({
-                redirect: '/blogs',
-            })
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+router.get('/blogs', blogController.blog_index);
+router.post('/blogs', blogController.blog_create_post);
+router.get('/blogs/create', blogController.blog_create_get);
+router.get('/blogs/:id', blogController.blog_details);
+router.delete('/blogs/:id', blogController.blog_delete);
 // trial delete using get request
-router.get('/blogsdelete/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+router.get('/blogsdelete/:id', blogController.blog_delete_trial_get);
 // trial delete using get request
 
 module.exports = router;
